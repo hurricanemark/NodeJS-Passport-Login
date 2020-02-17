b5f51c7ff73cbdc4d162c95cc5f7733fb67fbf23
Mark Nguyen <marknltv@comcast.net>
2020-02-16 16:10:24 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,73 @@
+# Project: Node.js Passport Login 
+
+## Title: PassportLogin
+
+## Description
+
+This code base is a simplified structure of Login using PassportJS.  It demonstrate the implementation of passport with expressJS, NodeJS, EJS.  It is without CSS.  It is without the backend database, content management.  
+
+Where to go from here?  Obviously, we could employ styling and data modelign, then on to JWT.
+
+## References
+
+[EJS!](https://ejs.co/#about)
+
+[ExpressJS!](https://expressjs.com/)
+
+[nodejs!](https://nodejs.org/en/)
+
+[bscript or bscryptjs!](https://passwordhashing.com/BCrypt)
+*bcrypt is a password hashing function designed by Niels Provos and David Mazières, based on the Blowfish cipher, and presented at USENIX in 1999.*[^]
+
+[passportJS!](http://www.passportjs.org/packages/passport-local/)
+
+
+# Setting Started
+
+** Environment ** vsnode
+[^] You need to  install extension for 
+EJS Language Support!  EJS is the Embedded JavaScript templating language that lets you generate HTML markup with plain JavaScript.
+
+`npm init`
+
+`npm i express ejs`
+
+`npm i --save-dev nodemon dotenv`
+
+[^] *bcrypt is used for hashing passwords, ...*
+
+`npm i bcrypt`  
+
+[^] *passport passport-local are used for password authentication.  express-session is for local session, express-flash displays invalid authentication messages*
+
+
+`npm i passport passport-local express-session express-flash`
+
+*Logic for passport,...,express-flash will be written in passport-config.js*[^]
+
+
+`npm i method-override`
+
+[^]*method-override alternates POST so we can call app.delete to implement logout*
+
+Then,
+
+- Create the following files:  .env .gitignore
+
+- Insert into .gitignore
+
+```
+.env
+
+node_modules
+```
+
+
+- Edit package.json
+
+`"scripts": { "devStart": "nodemon server.js" }`
+
+- To start the express server on the terminal:
+
+`npm run devStart`
+
---
+++ server.js
@@ -0,0 +1,105 @@
+/* 	
+first load all environment variables onto the development 
+environment, if not in production mode. 
+*/
+
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config()
+}
+
+
+const express = require('express')
+const bcrypt = require('bcrypt')
+const flash = require('express-flash')
+const session = require('express-session')
+const passport = require('passport')
+const methodOverride = require('method-override')
+const initializePassport = require('./passport_config')
+
+initializePassport(
+	passport, 
+	email => users.find(user => user.email === email),
+	id => users.find(user => user.id === id)
+)
+
+
+
+/* Todo: create model and migrate to database */
+/* 
+For now, declare local variables to store data instead of database.
+Important: NO in production.  Data goes away at end of session.
+ */
+const users = []
+
+const app = express()
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extended: false}))
+app.use(flash())  // manage messages
+app.use(session({
+	secret: process.env.SESSION_SECRET,
+	resave: false,
+	saveUninitialized: false
+}))
+
+app.use(passport.initialize())  //encrypt passport use for password
+app.use(passport.session())  // persist authenticated passport through out the session
+app.use(methodOverride('_method'))
+
+app.get('/', checkAuthenticated, (req, res) => {
+	console.log(req.user.name)
+	res.render('index.ejs', { name: req.user.name })
+})
+
+app.get('/login', checkNotAuthenticated, (req, res) => {
+	res.render('login.ejs')
+})
+
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+	successRedirect: '/',
+	failureRedirect: '/login',
+	failureFlash: true
+}))
+
+app.get('/register', checkNotAuthenticated, (req, res) => {
+	res.render('register.ejs')
+})
+
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+	try {
+		const hashedPassword = await bcrypt.hash(req.body.password, 10)
+		users.push({
+			id: Date.now().toString(),
+			name: req.body.name,
+			email: req.body.email,
+			password: hashedPassword
+		})
+		console.log("Latet name in registry: ", req.body.name)
+		res.redirect('/login')
+	} catch {
+		res.redirect('/register')
+
+	}
+	console.log(users)
+})
+
+
+function checkAuthenticated(req, res, next) {
+	if (req.isAuthenticated()) {
+		return next()
+	}
+
+	res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+	if (req.isAuthenticated()) {
+		return res.redirect('/')
+	}
+	next()	
+}
+
+app.delete('/logout', (req, res) => {
+	req.logout() //passport takes care of this.  We donot need to implement it.
+	res.redirect('/login')
+})
+app.listen(3000)
